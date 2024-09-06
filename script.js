9b6033e177a6da044c7317e8c13348bb5e39e395
Paul Jose <pauljose513@gmail.com>
2024-09-06 17:00:07 +0530

    code sumbitted

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
-const regex_1 = /^(\+1\s?)?(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}$/;
+const regex_1 = /^(\+?1\s?)?(\(\d{3}\)|\d{3})[\s.-]?\d{3}[\s.-]?\d{4}$/;
 const validityChecker = phoneNumber => {
     if(phoneNumber === "") {
         window.alert("Please provide a phone number");
     } else if(regex_1.test(phoneNumber)) {
-        // window.alert("Valid " + phoneNumber);
         document.getElementById('results-div').innerText = `Valid US number: ${phoneNumber}`;
     } else {
         document.getElementById('results-div').innerText = `Invalid US number: ${phoneNumber}`;
