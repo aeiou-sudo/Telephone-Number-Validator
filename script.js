0db3bc5143a60a29eaf71d18756801cf92a50a2f
Paul Jose <pauljose513@gmail.com>
2024-09-06 16:43:01 +0530

    html, css and js finished with working logic

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const output_message = document.getElementById('results-div');
-const clear_button = document.getElementById('clear-btn');
 
+const regex_1 = /^(\+1\s?)?(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}$/;
 const validityChecker = phoneNumber => {
     if(phoneNumber === "") {
         window.alert("Please provide a phone number");
+    } else if(regex_1.test(phoneNumber)) {
+        // window.alert("Valid " + phoneNumber);
+        document.getElementById('results-div').innerText = `Valid US number: ${phoneNumber}`;
+    } else {
+        document.getElementById('results-div').innerText = `Invalid US number: ${phoneNumber}`;
     }
+}
+
+const clearOutput = () => {
+    document.getElementById('results-div').innerText = "";
+    document.getElementById('user-input').value = "";
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -16,5 +25,8 @@ document.addEventListener('DOMContentLoaded', function() {
         if(e.key === 'Enter') {
             validityChecker(document.getElementById('user-input').value);
         }
+    });
+    document.getElementById('clear-btn').addEventListener('click', () => {
+        clearOutput();
     })
 });
